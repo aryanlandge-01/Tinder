d499258d17c181e3912385b5af0f194537cce4bc
aryanlandge-01 <landgearyan0@gmail.com>
2024-11-01 16:05:14 +0530

    start of project

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/hello",(req,res) => {
+    res.send(
+        "<h1>hello from the server.</h1>"
+    )
+})
+
+app.use("/",(req,res) => {
+    res.send("hello hello hello!");
+})
+
+app.listen(3000,() => {
+    console.log("Server is listening on port 3000");
+});
